bb00e941f2da6ddb0e5e68c3bf9b89adfa1cd25a
KevinHoogendam <khoogend1@student.avans.nl>
2016-10-13 14:49:00 +0200

    nieuwe ninja

---
+++ WpfNinja/Ninja.Database/Scripts/Seed/Ninja.sql
@@ -0,0 +1,12 @@
+﻿MERGE INTO dbo.Ninja AS Target  
+USING (values 
+	(1, 'Ninja', 1000)
+) AS Source (Id, Name, Gold)  
+ON Target.Id = Source.Id  
+WHEN NOT MATCHED BY TARGET THEN  
+	INSERT (Id, Name, Gold)  
+	VALUES (Id, Name, Gold)  
+WHEN MATCHED THEN
+	UPDATE SET
+		Name = Source.Name,
+		Gold = Source.Gold;
---
+++ WpfNinja/Ninja.Database/obj/Debug/postdeploy.sql
@@ -0,0 +1,25 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+MERGE INTO dbo.Ninja AS Target  
+USING (values 
+	(1, 'Ninja', 1000)
+) AS Source (Id, Name, Gold)  
+ON Target.Id = Source.Id  
+WHEN NOT MATCHED BY TARGET THEN  
+	INSERT (Id, Name, Gold)  
+	VALUES (Id, Name, Gold)  
+WHEN MATCHED THEN
+	UPDATE SET
+		Name = Source.Name,
+		Gold = Source.Gold;
+GO
